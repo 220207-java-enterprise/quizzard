26c72d3fca782dbae92768990657496bf7a03f62
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-02-10 12:21:18 -0600

    add new method for validation and ideas for how to go about validating each field

--- src/com/revature/quizzard/QuizzardDriver.java
+++ src/com/revature/quizzard/QuizzardDriver.java
@@ -8,6 +8,7 @@ public class QuizzardDriver {
     private static int loopCounter = 0;
 
     public static void main(String[] args) {
+        // TODO: at the start of the program, read in our users file, and store them in some sort of container, or Data Structure
 
         if (loopCounter == 3) {
             throw new RuntimeException("Looped three times");
@@ -25,6 +26,8 @@ public class QuizzardDriver {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
+
+
 
             String userSelection = consoleReader.readLine();
             System.out.println(userSelection);
@@ -52,20 +55,24 @@ public class QuizzardDriver {
                     System.out.print("Password: ");
                     String password = consoleReader.readLine();
 
-                    AppUser newUser = new AppUser(firstName, lastName, email, username, password);
-                    System.out.printf("Registration info provided: %s\n", newUser);
+                    System.out.print("Password Again: ");
+                    String password2 = consoleReader.readLine();
+
+
+
 
                     // TODO validate that good info was given
+                    AppUser newUser = new AppUser(firstName, lastName, email, username, password);
+                    if(isValidUser(newUser)){
+                        System.out.printf("Registration info provided: %s\n", newUser);
+                        newUser.setId(UUID.randomUUID().toString());
+                        String fileString = newUser.toString() + "\n";
 
-                    // TODO persist user info to a file
-                    newUser.setId(UUID.randomUUID().toString());
-                    String fileString = newUser.toFileString() + "\n";
-
-                    File usersDataFile = new File("data/users.txt");
-                    FileWriter dataWriter = new FileWriter(usersDataFile, true);
-                    dataWriter.write(fileString);
-                    dataWriter.close();
-
+                        File usersDataFile = new File("data/users.txt");
+                        FileWriter dataWriter = new FileWriter(usersDataFile, true);
+                        dataWriter.write(fileString);
+                        dataWriter.close();
+                    }
                     break;
                 case "3":
                     System.out.println("You selected: Exit");
@@ -79,9 +86,52 @@ public class QuizzardDriver {
             e.printStackTrace();
         }
 
+
         loopCounter++;
         main(args); // TODO maybe don't use recursion here?
-
     }
 
+
+    public static boolean isValidUser(AppUser user){
+        // Validation Criteria
+        // firstName -> trim the string, no integers, range, no strange characters, check for capital
+        // and that data was entered(if length != 0)
+        String firstName = user.getFirstName();
+        firstName = firstName.trim();
+
+        if(firstName.length() < 2 || firstName.length() > 15){
+            System.out.println("First name needs to be between 2 and 15 characters.");
+            return false;
+        }
+
+        for(int i = 0; i < firstName.length(); i++){
+            char c = firstName.charAt(i);
+            if(!Character.isAlphabetic(c)){
+                System.out.println("Name can not contain non-alpha characters");
+                return false;
+            }
+        }
+
+        // brandon -> b
+        String firstLetter = String.valueOf(firstName.charAt(0));
+        // b = B
+        firstLetter = firstLetter.toUpperCase();
+
+        // brandon = B + randon
+        firstName = firstLetter + firstName.substring(1);
+        System.out.println(firstName);
+
+
+
+
+
+        // lastName -> trim the string, no integers, range, no strange characters, check for capital
+        // and that data was entered(if length != 0)
+        // email -> check if exists, no more than 1 @ followed a character sequence followed by at least one .
+        // followed by some character sequence, check if uniqueness
+        // username -> begins with (char, underscore, or $), check for uniqueness(tabled until further notice)
+        // password -> exists, with no spaces, range?, check against "bad" pass, enter it twice
+        // password2 -> check that it matches password (this should happen first to trigger short-circuiting)
+        return false;
+    }
 }
