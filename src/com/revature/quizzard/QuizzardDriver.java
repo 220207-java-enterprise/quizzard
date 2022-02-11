2350a91ee2b5da8f3336a7810ef9615633c364c9
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-02-11 11:22:29 -0600

    add validation for registration information

--- src/com/revature/quizzard/QuizzardDriver.java
+++ src/com/revature/quizzard/QuizzardDriver.java
@@ -62,6 +62,11 @@ public class QuizzardDriver {
 
 
                     // TODO validate that good info was given
+                    // if the passwords don't match, break immediately before doing any other validation
+                    if(!password.equals(password2)){
+                        System.out.println("Your passwords must match.");
+                        break;
+                    }
                     AppUser newUser = new AppUser(firstName, lastName, email, username, password);
                     if(isValidUser(newUser)){
                         System.out.printf("Registration info provided: %s\n", newUser);
@@ -96,42 +101,132 @@ public class QuizzardDriver {
         // Validation Criteria
         // firstName -> trim the string, no integers, range, no strange characters, check for capital
         // and that data was entered(if length != 0)
-        String firstName = user.getFirstName();
-        firstName = firstName.trim();
 
-        if(firstName.length() < 2 || firstName.length() > 15){
-            System.out.println("First name needs to be between 2 and 15 characters.");
+
+        // check if firstName is valid
+        if(!isValidName(user.getFirstName())) return false;
+        // auto capitalize their first letter
+        String firstCapitalized = capitalize(user.getFirstName());
+        user.setFirstName(firstCapitalized);
+
+        // lastName -> trim the string, no integers, range, no strange characters, check for capital
+        // and that data was entered(if length != 0)
+
+        // check if lastName is valid
+        if(!isValidName(user.getLastName())) return false;
+        String lastCapitalized = capitalize(user.getLastName());
+        user.setLastName(lastCapitalized);
+
+        // email -> check if exists, no more than 1 @ followed a character sequence followed by at least one .
+        // followed by some character sequence, check if uniqueness
+        if(!isValidEmail(user.getEmail())) return false;
+
+        // username -> begins with (char, underscore, or $), check for uniqueness(tabled until further notice)
+        if(!isValidUsername(user.getUsername())) return false;
+
+        // password -> exists, with no spaces, range?, check against "bad" pass, enter it twice
+        // password2 -> check that it matches password (this should happen first to trigger short-circuiting)
+        if(!isValidPassword(user.getPassword())) return false;
+
+        // if they pass all the other checks, it is valid
+        return true;
+    }
+
+    private static boolean isValidPassword(String password) {
+        password = password.trim();
+
+        // check the range first
+        if(password.length() < 8 || password.length() > 24){
+            System.out.println("Invalid Password: Password must be between 8 and 24 characters.");
             return false;
         }
 
-        for(int i = 0; i < firstName.length(); i++){
-            char c = firstName.charAt(i);
-            if(!Character.isAlphabetic(c)){
-                System.out.println("Name can not contain non-alpha characters");
+        // check for spaces
+        for(int i = 0; i < password.length(); i++){
+            if(password.charAt(i) == ' '){
+                System.out.println("Invalid Password: Password must not contain spaces.");
                 return false;
             }
         }
 
+        return true;
+    }
+
+    private static boolean isValidUsername(String username) {
+        char firstLetter = username.charAt(0);
+
+        // check that it's a character or underscore, or $
+        if(!(Character.isAlphabetic(firstLetter) || firstLetter == '_' || firstLetter == '$')){
+            System.out.println("Invalid username: must start with a character, underscore, or dollar sign.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isValidName(String name){
+        name = name.trim();
+
+        if(name.length() < 2 || name.length() > 15){
+            System.out.println("Invalid Name: Name must be between 2 and 15 characters");
+            return false;
+        }
+
+        for(int i = 0; i < name.length(); i++){
+            char c = name.charAt(i);
+            if(!Character.isAlphabetic(c)){
+                System.out.println("Invalid Name: Name can not contain non-alpha characters");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isValidEmail(String email){
+        email = email.trim();
+
+        // check that there is one and only one @ symbol
+        // take the length of the initial string, and subtract the length of the substring with all the @'s removed
+        //                                          replace all instances of @ with nothing
+        int symbolCount = email.length() - email.replaceAll("@", "").length();
+        if(symbolCount != 1){
+            System.out.println("Invalid email: Cannot contain more than one @ symbol.");
+            return false;
+        }
+
+        // now that we know there is only one @ symbol, we need to check that it is not the first character
+        if(email.indexOf("@") == 0){
+            System.out.println("Invalid email: Cannot start with \"@\" symbol.");
+            return false;
+        }
+
+        // now we need to check that there is a period occurring after the @ symbol
+        // so we check the substring following the @ index to see if it contains a period
+        String afterAtSymbol = email.substring(email.indexOf("@"));
+        if(!afterAtSymbol.contains(".")){
+            System.out.println("Invalid email: Must contain a domain following the \"@\" symbol");
+            return false;
+        }
+
+        // We should also check that the . does not occur as the first index in the afterAtSymbol substring
+        if(afterAtSymbol.indexOf(".") == 1){
+            System.out.println("Invalid email: \"@\" symbol cannot be directly followed by a \".\"");
+            return false;
+        }
+
+        // if all the above pass, it is valid
+        return true;
+    }
+
+    private static String capitalize(String string){
         // brandon -> b
-        String firstLetter = String.valueOf(firstName.charAt(0));
+        String firstLetter = String.valueOf(string.charAt(0));
         // b = B
         firstLetter = firstLetter.toUpperCase();
 
         // brandon = B + randon
-        firstName = firstLetter + firstName.substring(1);
-        System.out.println(firstName);
-
-
-
-
-
-        // lastName -> trim the string, no integers, range, no strange characters, check for capital
-        // and that data was entered(if length != 0)
-        // email -> check if exists, no more than 1 @ followed a character sequence followed by at least one .
-        // followed by some character sequence, check if uniqueness
-        // username -> begins with (char, underscore, or $), check for uniqueness(tabled until further notice)
-        // password -> exists, with no spaces, range?, check against "bad" pass, enter it twice
-        // password2 -> check that it matches password (this should happen first to trigger short-circuiting)
-        return false;
+        string = firstLetter + string.substring(1);
+        return string;
     }
 }
